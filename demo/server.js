8de20819c16f43dec1520de3331e96ba937276f8
Christian Nally <sticksallison@gmail.com>
2021-01-17 22:13:15 -0800

    refactored to a switch statement and added a catch-all default

--- demo/server.js
+++ demo/server.js
@@ -8,6 +8,9 @@
 // https://nodejs.org/api/http.html
 // 
 
+// here is a curl command to make a POST request of this server
+// curl -X POST -H "Content-Type: application/json" -d '{"type":"chores", "description":"walk the dog"}' http://localhost:3000/todos
+
 // define the constants used throughout this file
 const http = require('http');
 const port = 3000;
@@ -15,17 +18,32 @@ const port = 3000;
 // create the web server
 // this function CAN be used by passing it a single parameter... a callback (called a requestListener) which has two parameters: request and response
 const server = http.createServer((req, res) => {
-    // req and res are objects that contain information about the request and response
-    console.log('requestListener was called');
-    console.log(`method: ${req.method}`);
-    console.log(`path: ${req.url}`);
+  // req and res are objects that contain information about the request and response
+  console.log('requestListener was called');
+  console.log(`method: ${req.method}`);
+  console.log(`path: ${req.url}`);
 
-    // depending on the response, build an appropriate response
-    if (req.method === 'GET' && req.url === '/'){
-        res.statusCode = 200; // 200 means OK
-        res.write("Homepage!"); // use the .write method to output text into the response.
-        res.end(); // use .end to finish the response.
-    }
+  // create route string
+  const route = `${req.method} ${req.url}`;
+
+  // depending on the response, build an appropriate response
+  switch (route) {
+    case 'GET /':
+      res.statusCode = 200; // 200 means OK
+      res.write("Homepage!"); // use the .write method to output text into the response.
+      res.end(); // use .end to finish the response.
+    break;
+    case 'GET /todos':
+      res.statusCode = 200; // 200 means OK
+      res.write("ToDos!");
+      res.end();
+    break;
+    default:
+      res.statusCode = 404; // 404 means Not Found
+      res.write("Cannot find this resource.");
+      res.end();
+    break;
+  }
 });
 
 // have the web server listen for incoming requests
