5be0c9f57afce31420da3282f4e5f977bdde769b
Christian Nally <sticksallison@gmail.com>
2021-01-17 23:47:56 -0800

    added express homepage route

--- demo/express/server.js
+++ demo/express/server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const port = 3000;
 
 // create an express app
+const app = express();
 
 // routing
+app.get('/',(req,res)=>{
+    console.log('Homepage Route called.');
+    res.write('Homepage!');
+    res.end();
+});
 
 // have the app listen on the port
+app.listen(port,console.log(`Server is listening on port ${port}`));
