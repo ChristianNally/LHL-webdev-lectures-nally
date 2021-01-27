72e3829bf6e93c67141b58857b0d0b78e3457a25
Christian Nally <sticksallison@gmail.com>
2021-01-27 09:07:48 -0800

    defining the fake url

--- w04d3/public/js/ajax-example.js
+++ w04d3/public/js/ajax-example.js
@@ -3,11 +3,16 @@ $(document).ready(function () {
   $("form").on("submit", function (event) {
     event.preventDefault();
   
+    let url = "example.com";
+
     $.ajax({
       url: url,
       method: "GET",
     }).then((result) => {
       console.log('ajax callback called');
+    }).catch(err => {
+      console.log('ajax error caught');
+      console.log(err); // bum related error
     });
 
   });
