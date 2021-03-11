43bed0b8930321a6caa081ebadbbf4e60d9b98a3
Christian Nally <sticksallison@gmail.com>
2021-03-11 07:59:18 -0800

    client and server are having an issue with multiple clients

--- w02d3/solution/client.js
+++ w02d3/solution/client.js
@@ -1,14 +1,43 @@
+const faker = require('faker');
+// import the net library
 const net = require('net');
-const stdin = process.stdin;
-//We need to specify the address and the port to connect to
+const port = 8000;
+// Create the connection to the server
 const client = net.createConnection({
+  port: port,
+  // host: '127.0.0.1'
   host: 'localhost',
-  port: 3000
 });
-//We need the encoding to tell the server and the client what kind of data are we transfering
-client.setEncoding('utf8');
-//When I, the client finally connect...........
-client.on('connect', function() {
-  client.write('I have connected!!!');
-})
 
+// set the encoding to utf-8
+client.setEncoding('utf8');
+
+// catch what we type on the keyboard using standard input
+process.stdin.on('data', function(message) {
+
+  client.write(message);
+
+});
+
+// Events: connect, message, error, end
+client.on('connect', function () {
+  console.log('client is connected to server');
+  const clientName = faker.name.findName();
+  client.write(`setName ${clientName}`);
+
+});
+
+// event handle for receiving incoming message from server
+client.on('data', function (message) {
+  console.log(message);
+});
+
+// event handler for error
+client.on('error', function (err) {
+  console.log(`Error: ${err.message}`);
+});
+
+// event handler for disconnection
+client.on('end', function () {
+  console.log('client disconnected from server');
+});
--- w02d3/solution/server.js
+++ w02d3/solution/server.js
@@ -1,16 +1,77 @@
+// import the net library -> TCP protocal
 const net = require('net');
-const stdin = process.stdin;
+// define the port on which the server will listen
+const port = 8000;
 
+// create the net server
+//Create an instance of the server
 const server = net.createServer();
-// when someone connects................
-server.on('connection', function(conn) {
-  //Set encoding just like on the client
-  conn.setEncoding('utf8');
-  //when one of the connections sends data and we recieve it.............
-  conn.on('data', function(data) {
-    console.log("connected user says:", data);
-  })
-  console.log("someone has connected");
-})
 
-server.listen(3000)
+// An array to keep tracks of all the connected clients
+
+const connectedClients = [];
+
+// broadcast the message to all connected clients
+
+const broadcast = function (currentClient, message) {
+  for (let connectedClient of connectedClients) {
+    // don t send it back the the issuing client
+    if (connectedClient !== currentClient) {
+      connectedClient.write(`${connectedClient.name} says: ${message}`);
+    }
+  }
+};
+
+// Wait for a few events: connection, message, errror, disconnect
+
+// Event: when a client connect => on connection
+server.on('connection', function (client) {
+  // client variable is an object containing all the properties of the client connection
+  console.log('Client is connected.');
+
+  // add the current client to the list of connected clients
+  connectedClients.push(client);
+
+  // set the encoding to utf8
+
+  client.setEncoding('utf8');
+
+  // sending back a welcome message
+
+  client.write('Welcome to my awesome server!');
+
+  // event handlder to catch any incoming client message
+
+  client.on('data', function (message) {
+    console.log(`Message received from client: ${message}`);
+
+    // are we getting a setName instruction?
+
+    if (message.match(/setName/)) {
+      const clientName = message.replace(/setName /, '');
+
+      client.name = clientName;
+
+    }
+
+
+    // send back the message to all connected clients > broadcast
+    broadcast(client, message);
+  });
+
+  // error event handler
+  client.on('error', function (err) {
+    console.log(`Error: ${err.message}`);
+  });
+
+  // on close event handler -> whenever a client disconnect
+
+  client.on('close', function () {
+    console.log('client is disconected.');
+  });
+});
+
+// have the server listen for incoming client connection
+server.listen(port, function () {
+  console.log(`Server is listening on port ${port}`);
+});
