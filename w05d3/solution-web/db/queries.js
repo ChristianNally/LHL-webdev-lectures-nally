7565d675a81e90cec7ec58ae66989d3366029cb2
Christian Nally <sticksallison@gmail.com>
2021-03-30 23:16:26 -0700

    query best practice

--- w05d3/solution-web/db/queries.js
+++ w05d3/solution-web/db/queries.js
@@ -1,7 +1,7 @@
 const client = require('./connection');
 
 const getAllObjectives = (cb) => {
-  client.query('SELECT * FROM objectives ORDER BY id;')
+  client.query('SELECT id,type,question,answer,sort,day_id FROM objectives ORDER BY id;')
     .then((response) => {
       // console.log(response.rows);
       cb(response.rows);
@@ -9,7 +9,7 @@ const getAllObjectives = (cb) => {
 };
 
 const getObjectiveById = (id) => {
-  return client.query('SELECT * FROM objectives WHERE id = $1;', [id])
+  return client.query('SELECT id,type,question,answer,sort,day_id FROM objectives WHERE id = $1;', [id])
     .then((response) => {
       return response.rows[0];
     })
