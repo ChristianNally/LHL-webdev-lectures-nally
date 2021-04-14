3b346807bca4b8d7783c4176c34354759a9a500c
Christian Nally <sticksallison@gmail.com>
2021-04-13 17:31:04 -0700

    touching up the chat client

--- w02d3/solution/client.js
+++ w02d3/solution/client.js
@@ -1,43 +1,26 @@
-const faker = require('faker');
-// import the net library
 const net = require('net');
-const port = 8000;
-// Create the connection to the server
+const port = 8008;
+
 const client = net.createConnection({
-  port: port,
-  // host: '127.0.0.1'
-  host: 'localhost',
+    port: port, 
+    host: 'localhost'
 });
 
 // set the encoding to utf-8
 client.setEncoding('utf8');
 
-// catch what we type on the keyboard using standard input
-process.stdin.on('data', function(message) {
-
-  client.write(message);
-
+process.stdin.on('data', function(message){
+    client.write(message);
 });
 
-// Events: connect, message, error, end
-client.on('connect', function () {
-  console.log('client is connected to server');
-  const clientName = faker.name.findName();
-  client.write(`setName ${clientName}`);
-
+client.on('connect',function(){
+    console.log("client is connected to server.");
 });
 
-// event handle for receiving incoming message from server
-client.on('data', function (message) {
-  console.log(message);
+client.on('data',function(message){
+    console.log('server sent:',message);
 });
 
-// event handler for error
-client.on('error', function (err) {
-  console.log(`Error: ${err.message}`);
-});
-
-// event handler for disconnection
-client.on('end', function () {
-  console.log('client disconnected from server');
+client.on('end', function(){
+    console.log('client disconnected from server');
 });
--- w02d3/solution/server.js
+++ w02d3/solution/server.js
@@ -1,77 +1,44 @@
-// import the net library -> TCP protocal
 const net = require('net');
-// define the port on which the server will listen
-const port = 8000;
+const port = 8008;
 
-// create the net server
-//Create an instance of the server
 const server = net.createServer();
 
-// An array to keep tracks of all the connected clients
-
+// we need an array to store the connected clients.
 const connectedClients = [];
 
-// broadcast the message to all connected clients
-
-const broadcast = function (currentClient, message) {
-  for (let connectedClient of connectedClients) {
-    // don t send it back the the issuing client
-    if (connectedClient !== currentClient) {
-      connectedClient.write(`${connectedClient.name} says: ${message}`);
+const broadcast = function(currentClient,message){
+    for (let connectedClient of connectedClients){
+        console.log('testing client named:',connectedclient.name);
+        if(connectedClient !== currentClient){
+            connectedClient.write(`${currentClient.name} says: ${message}`);
+        }
     }
-  }
 };
 
-// Wait for a few events: connection, message, errror, disconnect
+server.on('connection', function(client){
+    console.log('Client is connected');
 
-// Event: when a client connect => on connection
-server.on('connection', function (client) {
-  // client variable is an object containing all the properties of the client connection
-  console.log('Client is connected.');
+    // add the current client to the list of connected clients
+    connectedClients.push(client);
 
-  // add the current client to the list of connected clients
-  connectedClients.push(client);
+    // set the encoding to utf8
+    client.setEncoding('utf8');
 
-  // set the encoding to utf8
+    client.write("Welcome to my awesome server! ⛵");
 
-  client.setEncoding('utf8');
+    client.on('data', function(message){
+        console.log(`Message received from client: ${message}`);
+        if (message.startsWith('setName ')){
+            const clientName = message.replace(/setName /, '');
+            console.log('setting client name to:',clientName);
+            client.name = clientName;
+        }
+        broadcast(client,message);
+    });
 
-  // sending back a welcome message
-
-  client.write('Welcome to my awesome server!');
-
-  // event handlder to catch any incoming client message
-
-  client.on('data', function (message) {
-    console.log(`Message received from client: ${message}`);
-
-    // are we getting a setName instruction?
-
-    if (message.match(/setName/)) {
-      const clientName = message.replace(/setName /, '');
-
-      client.name = clientName;
-
-    }
-
-
-    // send back the message to all connected clients > broadcast
-    broadcast(client, message);
-  });
-
-  // error event handler
-  client.on('error', function (err) {
-    console.log(`Error: ${err.message}`);
-  });
-
-  // on close event handler -> whenever a client disconnect
-
-  client.on('close', function () {
-    console.log('client is disconected.');
-  });
 });
 
-// have the server listen for incoming client connection
-server.listen(port, function () {
-  console.log(`Server is listening on port ${port}`);
+// have the server listen to a particular port for incoming connections
+server.listen(port, function(){
+    console.log(`Server is listening on port ${port}`);
 });
