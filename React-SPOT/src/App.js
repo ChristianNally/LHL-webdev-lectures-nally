7f7bcb349fbc3856ad433a48be233bb28f830ed7
Christian Nally <sticksallison@gmail.com>
2021-06-07 16:16:15 -0700

    Andy's fixes to useEffect, proxying the API call, adding the gathering of objectives

--- React-SPOT/src/App.js
+++ React-SPOT/src/App.js
@@ -1,58 +1,60 @@
 import "./App.css";
-import {useState} from "react";
-import {useEffect} from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-//import axios from 'axios';
+import axios from 'axios';
 
-import ObjectivesSearchFrom from './Components/ObjectivesSearchForm';
+import ObjectivesSearchFrom from "./Components/ObjectivesSearchForm";
 import ObjectivesList from "./Components/ObjectivesList";
 
-let objectives = require('./objectives.json');
+let objectives = require("./objectives.json");
 // let objectives = [];
 
 function App() {
   console.log("App function executed.");
 
-  // useEffect(() => {
-  //   console.log("useEffect called");
+  useEffect(() => {
+    console.log("useEffect called");
 
-  //   const fakeObjectives = [
-  //     {
-  //     "id": 72,
-  //     "type": "learning",
-  //     "question": "What is Test Driven Development?",
-  //     "answer": "Tests are written before the code. The tests make the expectations of the code explicit. Red - Green - Refactor are the stages of code development. You make the code work (pass the tests!) and then you can refactor the code, safe in the knowledge that the code is relatively easily testable.",
-  //     "sort": 0,
-  //     "day_id": 6
-  //     },
-  //     {
-  //     "id": 73,
-  //     "type": "performance",
-  //     "question": "What is Mocha?",
-  //     "answer": "Mocha is a testing framework. It looks for test files to run in the ./test/ folder.",
-  //     "sort": 1,
-  //     "day_id": 6
-  //     },
-  //     {
-  //     "id": 74,
-  //     "type": "learning",
-  //     "question": "What is Chai?",
-  //     "answer": "Chai is an assertion library. It defines a large number of assertions useful for testing.",
-  //     "sort": 2,
-  //     "day_id": 6
-  //     },
-  //   ];
+    if (true){
+      fetch('/json')
+      .then(res => res.json())
+      .then(data => {
+        console.log("data:",data);
+        setSourceObjectives(data);
+        setFilteredObjectives(data);
+      })
+      .catch(err => console.log("err:",err));
+    } else {
+      axios.get('/json')
+      .then(res => {
+        console.log("data:",res.data);
+        setSourceObjectives(res.data);
+        setFilteredObjectives(res.data);
+      })
+      .catch(err => console.log("err:",err));  
+    }
+  }, []);
 
-  //   setSourceObjectives(fakeObjectives);
+  useEffect(() => {
+    console.log("useEffect 2 called");
 
-  //   // // retrieve user information from an API and update local state with the response
-  //   // fetch(`http://localhost:7865/json`)
-  //   //   .then(res => res.json())
-  //   //   .then(json => {
-  //   //     objectives = json;
-  //   //   })
-  //   //   .catch(err=>alert('err:'+err));
-  // },[]);
+    const newObjective = [
+      {
+        id: 772,
+        type: "learning",
+        question: "What is Test Driven Development?",
+        answer:
+          "Tests are written before the code. The tests make the expectations of the code explicit. Red - Green - Refactor are the stages of code development. You make the code work (pass the tests!) and then you can refactor the code, safe in the knowledge that the code is relatively easily testable.",
+        sort: 0,
+        day_id: 6,
+      },
+    ];
+    setTimeout(() => {
+      setSourceObjectives((source) => [...source, ...newObjective]);
+      newSearchDetails(searchDetails);
+    }, 5555);
+  }, []);
 
   // THIS IS NOT WORKING. CAN I CALL useEffect here?
   // axios.get(`http://localhost:7865/json`)
@@ -68,37 +70,52 @@ function App() {
     search: "",
   });
 
-  const [filteredObjectives,setFilteredObjectives] = useState(objectives);
-  const [sourceObjectives,setSourceObjectives] = useState([]);
+  const [sourceObjectives, setSourceObjectives] = useState(objectives);
+  const [filteredObjectives, setFilteredObjectives] = useState([
+    {
+      id: 74,
+      type: "learning",
+      question: "What is Chai Actually?",
+      answer:
+        "Chai is an assertion library. It defines a large number of assertions useful for testing.",
+      sort: 2,
+      day_id: 6,
+    },
+  ]);
 
   const newSearchDetails = (details) => {
-    setSearchDetails({...details});
-    const newObjectivesArray = [];
-    objectives.forEach((item)=>{
+    setSearchDetails({ ...details });
+  };
+
+  useEffect(() => {
+    const newObjectivesArray = sourceObjectives.filter((item) => {
       // console.log('item:',item);
       // console.log('details:',details);
       const calculatedWeek = Math.floor(item.day_id / 5) + 1;
-      const specifiedWeek = parseInt(details.week, 10);
+      const specifiedWeek = parseInt(searchDetails.week, 10);
       // console.log("calculatedWeek:",calculatedWeek);
       // console.log("specifiedWeek",specifiedWeek);
       // console.log(calculatedWeek === specifiedWeek);
       if (
-        ( item.type === details.type || details.type === 'all' )
-        &&
-        ( item.day_id % 5 === parseInt(details.day, 10) || details.day === 0)
-        &&
-        ( calculatedWeek === specifiedWeek || details.week === 0 )
-        ) {
-        newObjectivesArray.push(item);
+        (item.type === searchDetails.type || searchDetails.type === "all") &&
+        (item.day_id % 5 === parseInt(searchDetails.day, 10) ||
+          searchDetails.day === 0) &&
+        (calculatedWeek === specifiedWeek || searchDetails.week === 0)
+      ) {
+        return true;
       }
+      return false;
     });
     setFilteredObjectives(newObjectivesArray);
-  };
+  }, [sourceObjectives, searchDetails]);
 
   return (
     <div className="App">
       <h2>SPOTlight</h2>
-      <ObjectivesSearchFrom searchDetails={searchDetails} newSearchDetails={newSearchDetails} />
+      <ObjectivesSearchFrom
+        searchDetails={searchDetails}
+        newSearchDetails={newSearchDetails}
+      />
       <ObjectivesList objectives={filteredObjectives} />
     </div>
   );
