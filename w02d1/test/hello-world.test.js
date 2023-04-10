aa40e78a97ff85340d14f3207041817013b18b5f
Christian Nally <sticksallison@gmail.com>
2023-04-10 07:26:06 -0700

    deleting the prepared test directory

--- w02d1/test/hello-world.test.js
+++
@@ -1,8 +0,0 @@
-// pull in whichever assert library you'd like to use
-
-
-// pull in the main functions that will be tested
-
-
-// write the mocha describe() and it() function calls that will implement the test(s)
-
